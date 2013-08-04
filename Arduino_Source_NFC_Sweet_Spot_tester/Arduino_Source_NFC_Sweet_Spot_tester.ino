897db827a3ec6b7ff90c5d8a368445fb435050dc
John McLear <john@mclear.co.uk>
2013-08-04 13:47:34 +0100

    Working movement and reset

--- Arduino_Source_NFC_Sweet_Spot_tester/Arduino_Source_NFC_Sweet_Spot_tester.ino
+++ Arduino_Source_NFC_Sweet_Spot_tester/Arduino_Source_NFC_Sweet_Spot_tester.ino
@@ -1,34 +1,52 @@
+  /*******************************
+  *
+  * RESET THE MACHINE BACK TO 0,0 
+  *
+  ********************************/
 
-
-//Pin Definitions
+// motors (x and y)
 int pinXStep = 2;
 int pinXDir = 3;
-
 int pinYStep = 4;
 int pinYDir = 5;
 
-int pinEnable  = 8;
+// Switches that are triggered when max and min Y are reached
+int pinXSwitch = 12;
+int pinYSwitch = 11;
+
+int pinEnable  = 8; // not used...
 
 //Direction Variables
 int xCCW = LOW;
 int xCW = HIGH;
-
 int yCCW = LOW;
 int yCW = HIGH;
 
+int step = 10000; // The size of the step to take when resetting
+
 int stepSpeed = 50*5; //milliseconds delay between pulses
 
-long widthSteps = 105000/5;
-long heightSteps = 140000/5;
+long widthSteps = 105000;
+long heightSteps = 140000;
 
-long widthDivisions = 4;
-long heightDivisions = 4;
+long widthDivisions = 10;
+long heightDivisions = 10;
 
 int testDelay = 1000;
 int sampleDelay = 1000;
 
+int reset = 0; // has reset been completed?
+int xReset = 0; // is X reset?
+int yReset = 0; // is Y reset?
+
 void setup() {
-  delay(5000);
+  Serial.begin(9600);
+  Serial.println("Setup");
+  pinMode(pinXSwitch, INPUT);
+  pinMode(pinYSwitch, INPUT);
+ 
+
+  delay(500);
   pinMode(pinXStep, OUTPUT);
   pinMode(pinXDir, OUTPUT);
 
@@ -37,27 +55,81 @@ void setup() {
   
   pinMode(pinEnable, OUTPUT);  
   digitalWrite(pinEnable, HIGH);
+ 
+  delay(500);
+  
 
 }
 
 void loop() {
+  
+  /*******************************
+  *
+  * RESET THE MACHINE BACK TO 0,0 
+  *
+  ********************************/
+  
+  if (reset == 0){ // Should we be resetting the position?
+    // Until PinX Goes High Reset the motor
+    Serial.println("Resetting");
+    
+    if(xReset == 0){
+      Serial.println("Resetting X");
+      while(digitalRead(pinXSwitch) == 0){ // While the switch isn't depressed
+        Serial.println("Moving X back to 0");
+        moveX(-500);
+      }
+      if(digitalRead(pinXSwitch) == 1){
+        xReset = 1; 
+      }
+    }
+
+    if(yReset == 0){
+      Serial.println("Resetting Y");
+      Serial.println(digitalRead(pinYSwitch));
+      // Until PinY Goes High Reset the motor
+      while(digitalRead(pinYSwitch) == 0){ // While the switch isn't depressed
+        Serial.println("Moving Y back to 0");
+        moveY(-500);
+      }
+      if(digitalRead(pinYSwitch) == 1){
+        yReset = 1; 
+      }
+    }
+    reset = 1; // reset is now completed, yay :)
+  }
+  
+  /*******************************
+  *
+  * GO OVER THE TARGET DEVICE
+  *
+  ********************************/
+  
+
   for(int height = 0; height < heightDivisions; height++){
-     for(int width = 0; width <  widthDivisions; width++){
-       moveY(widthSteps/widthDivisions);
-       takeSample();      
-     }
-  moveY(-widthSteps); 
-  delay(testDelay);  
-  moveX(heightSteps/heightDivisions);
+    for(int width = 0; width <  widthDivisions; width++){
+      moveY(widthSteps/widthDivisions);
+      takeSample();      
+    }
+
+//    moveY(-widthSteps); 
+      while(digitalRead(pinYSwitch) == 0){ // While the switch isn't depressed
+        Serial.println("Moving Y back to 0");
+        moveY(-500);
+      }
+    delay(testDelay);  
+    moveX(heightSteps/heightDivisions);
     delay(testDelay);
   }    
- for(int width = 0; width <  widthDivisions; width++){
-       moveY(widthSteps/widthDivisions);
-       takeSample();      
- }
+  
+  for(int width = 0; width <  widthDivisions; width++){
+    moveY(widthSteps/widthDivisions);
+    takeSample();      
+  }
   moveY(-widthSteps);
   moveX(-heightSteps);
   delay(10000);
+  
 }
   
   
@@ -82,6 +154,8 @@ void moveY(long steps){
 } 
   
 void moveX(long steps){
+  Serial.print("Moving");
+  Serial.print(steps);
    if(steps < 0){
       digitalWrite(pinXDir, xCW);
    }
